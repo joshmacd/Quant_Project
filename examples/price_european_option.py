2778a43855c442f1071deff956cafcc69026370d
Josh MacDonald <71775942+joshmacd@users.noreply.github.com>
2026-06-04 17:19:13 +0100

    Added a comment at the end of the file to provide instructions on how to run the script.

--- examples/price_european_option.py
+++ examples/price_european_option.py
@@ -16,4 +16,5 @@ print(f"The price of the European put option is: {put_price:.2f}")
 
 
 #run the following command in the terminal:
-# PYTHONPATH=src python examples/price_european_option.py   
+# PYTHONPATH=src python examples/price_european_option.py
+# $env:PYTHONPATH="src";python examples/price_european_option.py
