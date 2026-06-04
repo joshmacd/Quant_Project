b312636ef017361de4edf577c04e1b41a3ec3c86
Josh MacDonald <71775942+joshmacd@users.noreply.github.com>
2026-06-04 16:30:35 +0100

    Final Changes

--- examples/price_european_option.py
+++ examples/price_european_option.py
@@ -3,9 +3,10 @@ from quant_project.black_scholes import put_option_price, call_option_price
 #Example variables
 S = 100  # Current stock price
 K = 100  # Strike price
-T=1    # Time to maturity (in years)
+T = 1    # Time to maturity (in years)
 r = 0.05  # Risk-free interest rate 
 sigma = 0.2  # Volatility of the stock
+
 # Calculate call option price
 call_price = call_option_price(S, K, r, sigma, T)
 print(f"The price of the European call option is: {call_price:.2f}")
